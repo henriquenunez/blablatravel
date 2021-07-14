ea7c08c8e8a544fbd6ccef8175900101b71156d7
Henrique Hiram Libutti Nunez <henrique.nunez@yahoo.com>
2021-07-13 21:44:55 -0300

    Login works

---
+++ src/sql/consultas.sql
@@ -0,0 +1,35 @@
+-- Avaliar um turista que participou de um dos seus passeios.
+
+-- Consultar passeios a partir de um filtro.
+
+SELECT * FROM
+
+-- O filtro pode ser: atrações próximas a um determinado local. ou sei la
+
+-- Todos:
+-- Busca por atrações
+-- Usuário:
+-- Turista:
+-- Adição de “Gostei” em sua lista.
+-- Adição de passeio. (inscrição em passeio).
+-- Consultar passeios a partir de um filtro.
+-- Enviar mensagem para outro usuário (guia ou turista).
+-- Criar um relato de bug na aplicação.
+-- Criar um relato de denúncia sobre outro usuário.
+-- Criar um relato de objeto perdido.
+-- Criar um relato genérico (outro).
+-- Guia:
+-- Criar um passeio.
+-- Consultar passeios cadastrados por si mesmo.
+-- Enviar mensagem para outro usuário (guia ou turista).
+-- Avaliar um turista que participou de um dos seus passeios.
+-- Criar um relato de bug na aplicação.
+-- Criar um relato de denúncia sobre outro usuário.
+-- Criar um relato genérico (outro).
+-- Funcionário:
+-- Buscar relatos dos usuários.
+-- Alterar o status de uma atração.
+-- Técnico:
+-- Marcar um relato de bug como resolvido.
+-- Atendente:
+-- Marcar o problema como resolvido.
